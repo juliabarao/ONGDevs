a2ecca5993c6d008c85d5cdcbe55db8ae1ddc1db
Júlia Barão <80726258+juliabarao@users.noreply.github.com>
2021-08-12 14:23:44 -0300

    Add files via upload

---
+++ ProjetoJavaBloco1/ProjetoJavaGeneration/src/ProjetoJavaGrupo06/Apresentacao.java
@@ -0,0 +1,19 @@
+package ProjetoJavaGrupo06;
+
+import java.util.Scanner;
+
+@SuppressWarnings("unused")
+public class Apresentacao {
+
+	public static void main(String[] args) {
+		
+	
+		Desenvolvimento desenv = new Desenvolvimento();
+		
+		desenv.ApresentacaoInicial();
+		
+		desenv.Apresentar();	
+
+	}
+
+}
